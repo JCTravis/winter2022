1510cf62f85673718b55a3d0decdbe42282b64e9
Jacob <jctravis@umich.edu>
2022-02-08 15:10:26 -0500

    Fixed brackets

--- Hello.java
+++ Hello.java
@@ -2,9 +2,9 @@ public class Hello
 {
 public static void main(String[] args)
 {
-for(int i=1;i<=10;i++)
+for(int i=1;i<=10;i++){
 for(int j=1;j<=11;j++){
-{
+
 System.out.println("Hello World!..."+i);
 }
 }
